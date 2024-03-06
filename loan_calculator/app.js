f7c00c8c11deb232a1dbcc0a70c435691c81ae92
Nathan Gross <nathan.gross@gmail.com>
2024-03-06 11:22:45 -0600

    Refactor to use 'request handlers' and async/await

--- loan_calculator/app.js
+++ loan_calculator/app.js
@@ -1,31 +1,31 @@
-const HTTP = require("http");
-const { URL, URLSearchParams } = require("url");
-const HANDLEBARS = require("handlebars");
-const { LoanTerms, LoanCalculator } = require("./loan_classes");
-const FS = require("fs");
-const PATH = require("path");
-const QUERYSTRING = require("querystring");
+const HTTP = require('http');
+const { URL } = require('url');
+const HANDLEBARS = require('handlebars');
+const FS = require('fs');
+const PATH = require('path');
+const QUERYSTRING = require('querystring');
+const { LoanTerms, LoanCalculator } = require('./loan_classes');
 
 const MIME_TYPES = {
-  ".css": "text/css",
-  ".js": "application/javascript",
-  ".jpg": "image/jpeg",
-  ".png": "image/png",
-  ".ico": "image/x-icon",
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.jpg': 'image/jpeg',
+  '.png': 'image/png',
+  '.ico': 'image/x-icon',
 };
 const PORT = 3000;
 const APR = 0.05;
 const percentFormat = new Intl.NumberFormat(
-  "en-US",
-  { style: "percent", minimumFractionDigits: 2 },
+  'en-US',
+  { style: 'percent', minimumFractionDigits: 2 },
 );
 const currencyFormat = new Intl.NumberFormat(
-  "en-US",
-  { style: "currency", currency: "USD" },
+  'en-US',
+  { style: 'currency', currency: 'USD' },
 );
 const currencyWholeFormat = new Intl.NumberFormat(
-  "en-US",
-  { style: "currency", currency: "USD", maximumFractionDigits: 0 },
+  'en-US',
+  { style: 'currency', currency: 'USD', maximumFractionDigits: 0 },
 );
 const ERROR_MESSAGE_400 = `
 <!DOCTYPE html>
@@ -46,7 +46,7 @@ const ERROR_MESSAGE_400 = `
 </body>
 </html>
 `;
-const unitFormat = new Intl.NumberFormat("en-US");
+const unitFormat = new Intl.NumberFormat('en-US');
 const RAW_TEMPLATE = `
 <!DOCTYPE html>
 <html lang="en">
@@ -73,11 +73,15 @@ const RAW_TEMPLATE = `
           <tr>
             <th>Duration:</th>
             <td>
-              <a href="{{durationDecrement.url}}">{{durationDecrement.label}}</a>
+              <a href="{{durationDecrement.url}}">
+                {{durationDecrement.label}}
+              </a>
             </td>
             <td>{{duration}} years</td>
             <td>
-              <a href="{{durationIncrement.url}}">{{durationIncrement.label}}</a>
+              <a href="{{durationIncrement.url}}">
+                {{durationIncrement.label}}
+              </a>
             </td>
           </tr>
           <tr>
@@ -110,7 +114,9 @@ const LOAN_FORM_SOURCE = `<!DOCTYPE html>
         <p>All loans are offered at an APR of {{apr}}%.</p>
         <label for="amount">How much do you want to borrow (in dollars)?</label>
         <input type="number" name="amount" id="amount" value="">
-        <label for="duration">How much time do you want to pay back your loan?</label>
+        <label for="duration">
+          How much time do you want to pay back your loan?
+        </label>
         <input type="number" name="duration" id="duration" value="">
         <input type="submit" name="" value="Get loan offer!">
       </form>
@@ -122,19 +128,19 @@ function render(template, data) {
   return HANDLEBARS.compile(template)(data);
 }
 
-/** @returns {URLSearchParams} */
 function getParams(path, host) {
   const myUrl = new URL(path, `http://${host}`);
   return myUrl.searchParams;
 }
 
-function getParsedFormData(request, callback) {
-  let body = "";
-  request.on("data", (chunk) => {
-    body += chunk.toString();
-  });
-  request.on("end", () => {
-    callback(QUERYSTRING.parse(body));
+function getParsedFormDataPromise(request) {
+  return new Promise((resolve) => {
+    let body = '';
+    request
+      .on('data', (chunk) => {
+        body += chunk.toString();
+      })
+      .on('end', () => resolve(QUERYSTRING.parse(body)));
   });
 }
 
@@ -147,34 +153,78 @@ function generateChangeLink(terms, paramToChange, delta) {
   const newTerms = { ...terms };
   newTerms[paramToChange] += delta;
   let formatter;
-  if (paramToChange === "amount") {
+  if (paramToChange === 'amount') {
     formatter = currencyWholeFormat;
-  } else if (paramToChange === "periodYears") {
+  } else if (paramToChange === 'periodYears') {
     formatter = unitFormat;
   } else {
     formatter = percentFormat;
   }
   return {
     url:
-      `/loan-offer?amount=${newTerms.amount}&duration=${newTerms.periodYears}&apr=${newTerms.apr}`,
-    label: `${(delta > 0) ? "+" : "-"}${formatter.format(Math.abs(delta))}`,
+      `/loan-offer?amount=${newTerms.amount}`
+      + `&duration=${newTerms.periodYears}`
+      + `&apr=${newTerms.apr}`,
+    label: `${(delta > 0) ? '+' : '-'}${formatter.format(Math.abs(delta))}`,
   };
 }
 
 function generateLoanOfferData(terms) {
   const calculator = new LoanCalculator(terms);
-  let data = {};
-  data.amountDecrement = generateChangeLink(terms, "amount", -100);
+  const data = {};
+  data.amountDecrement = generateChangeLink(terms, 'amount', -100);
   data.amount = currencyWholeFormat.format(terms.amount);
-  data.amountIncrement = generateChangeLink(terms, "amount", 100);
-  data.durationDecrement = generateChangeLink(terms, "periodYears", -1);
+  data.amountIncrement = generateChangeLink(terms, 'amount', 100);
+  data.durationDecrement = generateChangeLink(terms, 'periodYears', -1);
   data.duration = terms.periodYears;
-  data.durationIncrement = generateChangeLink(terms, "periodYears", 1);
-  data.aprDecrement = generateChangeLink(terms, "apr", -0.0025);
+  data.durationIncrement = generateChangeLink(terms, 'periodYears', 1);
+  data.aprDecrement = generateChangeLink(terms, 'apr', -0.0025);
   data.apr = percentFormat.format(terms.apr);
-  data.aprIncrement = generateChangeLink(terms, "apr", 0.0025);
+  data.aprIncrement = generateChangeLink(terms, 'apr', 0.0025);
   data.payment = currencyFormat.format(calculator.getMonthlyPayment());
   return data;
+}
+
+function respondIndex(res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(render(LOAN_FORM_SOURCE, { apr: '5' }));
+  res.end();
+}
+
+function generateLoanOfferResponse(res, terms) {
+  if (terms.amount && terms.periodYears) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(
+      render(RAW_TEMPLATE, generateLoanOfferData(terms)),
+    );
+  } else {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/html');
+    res.write(ERROR_MESSAGE_400);
+  }
+  res.end();
+}
+
+function respondLoanOfferGet(res, path, host) {
+  const params = getParams(path, host);
+  const terms = new LoanTerms(
+    Number(params.get('amount')),
+    Number(params.get('duration')),
+    Number(params.get('apr')) || APR,
+  );
+  generateLoanOfferResponse(res, terms);
+}
+
+async function respondLoanOfferPost(req, res) {
+  const query = await getParsedFormDataPromise(req);
+  const terms = new LoanTerms(
+    Number(query.amount),
+    Number(query.duration),
+    Number(query.apr) || APR,
+  );
+  generateLoanOfferResponse(res, terms);
 }
 
 const SERVER = HTTP.createServer((req, res) => {
@@ -182,58 +232,19 @@ const SERVER = HTTP.createServer((req, res) => {
   const { host } = req.headers;
   const pathname = getPathname(path, host);
   const fileExtension = PATH.extname(pathname);
-  const method = req.method;
+  const { method } = req;
 
-  if (pathname === "/") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.write(render(LOAN_FORM_SOURCE, { apr: "5" }));
-    res.end();
-  } else if (pathname === "/loan-offer") {
-    let terms;
-    if (method === "GET") {
-      const params = getParams(path, host);
-      terms = new LoanTerms(
-        Number(params.get("amount")),
-        Number(params.get("duration")),
-        Number(params.get("apr")) || APR,
-      );
-    } else if (method === "POST") {
-      let query;
-      getParsedFormData(req, (data) => query = data);
-      setTimeout(
-        () => {
-          terms = new LoanTerms(
-            Number(query.amount),
-            Number(query.duration),
-            Number(query.apr) || APR,
-          );
-        },
-        0,
-      );
-    }
-    setTimeout(
-      () => {
-        if (terms.amount && terms.periodYears) {
-          res.statusCode = 200;
-          res.setHeader("Content-Type", "text/html");
-          res.write(
-            render(RAW_TEMPLATE, generateLoanOfferData(terms)),
-          );
-        } else {
-          res.statusCode = 400;
-          res.setHeader("Content-Type", "text/html");
-          res.write(ERROR_MESSAGE_400);
-        }
-        res.end();
-      },
-      0,
-    );
+  if (pathname === '/') {
+    respondIndex(res);
+  } else if (pathname === '/loan-offer' && method === 'GET') {
+    respondLoanOfferGet(res, path, host);
+  } else if (pathname === '/loan-offer' && method === 'POST') {
+    respondLoanOfferPost(req, res);
   } else {
     FS.readFile(`./public/${pathname}`, (_, data) => {
       if (data) {
         res.statusCode = 200;
-        res.setHeader("Content-Type", `${MIME_TYPES[fileExtension]}`);
+        res.setHeader('Content-Type', `${MIME_TYPES[fileExtension]}`);
         res.write(`${data}\n`);
         res.end();
       } else {
